13259c51944fc246b69cd899b6be5d5dbcbeaf9e
Matin <shariatimatin@gmail.com>
2023-05-14 16:25:41 +0330

    Custom User Model Created with Customize admin page

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin, Group
+from .models import MyUser
 
-# Register your models here.
+
+@admin.register(MyUser)
+class MyUserAdmin(UserAdmin):
+
+    search_fields = ('email', 'username', 'first_name')
+    list_filter = ('email', 'username', 'is_active', 'is_staff')
+    ordering = ('-date_joined', )
+    list_display = ('username', 'email', 'phone_number', 'is_active', 'is_staff')
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'phone_number')}),
+        ('Profile', {'fields': ('first_name', 'last_name', 'profile_picture', 'biography', 'gender', 'birthday')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide', ),
+            'fields': ('username', 'email', 'phone_number', 'password1', 'password2',)}
+         ),
+        ('Profile', {'fields': ('first_name', 'last_name', 'profile_picture', 'biography', 'gender', 'birthday')}
+         ),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'groups', 'user_permissions')})
+    )
--- users/models.py
+++ users/models.py
@@ -1,7 +1,84 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, User
+from django.utils.translation import gettext_lazy as _
+from django.core import validators
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+
+
+class MyUserManager(BaseUserManager):
+
+    def create_user(self, email, username, password, **other_fields):
+        if not email:
+            raise ValueError(_('You must provide an email address'))
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username,  **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, username, phone_number, password, **other_fields):
+        other_fields.setdefault('is_active', True)
+        other_fields.setdefault('is_staff', True)
+        other_fields.setdefault('is_superuser', True)
+
+        if other_fields.get('is_staff') is not True:
+            raise ValueError(_("Super user must be assigned to is_staff=True."))
+
+        if other_fields.get('is_superuser') is not True:
+            raise ValueError(_("Super user must be assigned to is_superuser=True."))
+
+        return self.create_user(email, username, password, **other_fields)
 
 
 class MyUser(AbstractBaseUser, PermissionsMixin):
 
-    firstname
+    username_validator = UnicodeUsernameValidator()
+
+    GENDER_CHOICES = [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Others'),
+    ]
+
+    username = models.CharField(
+        _("username"),
+        max_length=50,
+        unique=True,
+        help_text=_("Required. 50 characters or fewer. Letters, digits and @/./+/-/_ only."),
+        validators=[username_validator],
+        error_messages={
+            'unique': _("A user with that username already exists."),
+        },
+    )
+    first_name = models.CharField(_('first name'), max_length=50, blank=True)
+    last_name = models.CharField(_('last name'), max_length=50, blank=True)
+    email = models.EmailField(_('email'), unique=True)
+    phone_number = models.BigIntegerField(_('phone number'), unique=True, blank=True, null=True,
+                                          validators=[
+                                              validators.RegexValidator(r'^989[0-3,9]\d{8}$',
+                                                                        _("Enter a valid mobile number."),
+                                                                        'invalid')])
+    is_staff = models.BooleanField(_('staff'), default=False)
+    is_active = models.BooleanField(_('active'), default=False)
+    date_joined = models.DateTimeField(_('date joined'), auto_now_add=True)
+    date_updated = models.DateTimeField(_('date updated'), auto_now=True)
+    profile_picture = models.ImageField(_('profile picture'), default='default.jpg', upload_to='profile_pictures')
+    biography = models.TextField(_('biography'), blank=True)
+    gender = models.CharField(_('gender'), choices=GENDER_CHOICES, blank=True, max_length=10)
+    birthday = models.DateField(_('birthday'), blank=True, null=True)
+
+    objects = MyUserManager()
+
+    EMAIL_FIELD = 'email'
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = 'MyUser'
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
